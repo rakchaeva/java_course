76617a7e586bfec02c91bc938090758d65940937
rakchaeva <k.rakchaeva@gmail.com>
2020-10-04 19:02:30 +0300

    Optimize navigation methods

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,19 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends BaseHelper {
-    private WebDriver wd;
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void goToGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
-    public void goToContactForm() { click(By.linkText("add new")); }
+    public void goToContactForm() {
+        click(By.linkText("add new"));
+    }
 
     public void goToHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
