ad08fa5f5fe826e32bd6ab67a0ea4dcfdfcfaa66
rakchaeva <k.rakchaeva@gmail.com>
2020-10-18 19:02:54 +0300

    Add Collections class

---
+++ sandbox/src/main/java/testPackage/Collections.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+        for (String l : languages) {
+            System.out.println("I want to learn " + l);
+        }
+    }
+}
